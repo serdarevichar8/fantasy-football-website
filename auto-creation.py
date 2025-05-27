431f37995596664908cb68d9dffedb357f20016b
serdarevichar8 <serdarevichar@gmail.com>
2025-05-27 16:30:32 -0500

    Made updates to navbar, moved it into the header added champion sheet

--- auto-creation.py
+++ auto-creation.py
@@ -46,7 +46,10 @@ def header(active_year: int = None):
 def topnav(active_year: int = None):
     container = div(_class='topnav')
 
-    home = a('Home', href=f'{root}/')
+    if active_year == 'Home':
+        home = div(a('Home', href=f'{root}/'), _class='dropdown active')
+    else:
+        home = div(a('Home', href=f'{root}/'), _class='dropdown')
     container.add(home)
 
     years_weeks = [(2019,13), (2020,13), (2021,14), (2022,14), (2023,14), (2024,14)]
@@ -68,7 +71,7 @@ def topnav(active_year: int = None):
         dropdown.add(dropdown_content)
         container.add(dropdown)
 
-    champions = a('Champions', href=f'{root}/champion.html')
+    champions = div(a('Champions', href=f'{root}/champion.html'), _class='dropdown')
     container.add(champions)
 
     return container
@@ -105,51 +108,72 @@ df = pd.read_csv('/Users/serdarevichar/Library/CloudStorage/GoogleDrive-serdarev
 #print(header(active_year=2019))
 
 
+def week_pages():
+    years = [2019,2020,2021,2022,2023,2024]
+
+    for year in years:
+        if year < 2021:
+            weeks = 13
+        else:
+            weeks = 14
+
+        for i in range(weeks):
+            # Construct the body of the HTML
+            doc = dominate.document(title='Fantasy Football')
+            doc.head.add(link(rel='stylesheet', href=f'{root}/style.css'))
 
 
-years = [2019,2020,2021,2022,2023,2024]
+            # Add in the pieces which make up the general template
+            doc.add(header(active_year=year))
+            scoreboard = df.loc[(df['Year'] == year) & (df['Week'] == i+1), ['Home Team','Home Score','Away Score','Away Team']].copy()
+            standings = df.loc[(df['Year'] == year) & (df['Week'] <= i+1)].copy()
 
-for year in years:
-    if year < 2021:
-        weeks = 13
-    else:
-        weeks = 14
+            temp1 = standings[['Home Team','Home Score','Away Score']].copy()
+            temp2 = standings[['Away Team','Away Score','Home Score']].copy()
 
-    for i in range(weeks):
-        # Construct the body of the HTML
-        doc = dominate.document(title='Fantasy Football')
-        doc.head.add(link(rel='stylesheet', href=f'{root}/style.css'))
+            temp1.rename(columns={'Home Team':'Team', 'Home Score':'Score', 'Away Score':'Opp Score'}, inplace=True)
+            temp2.rename(columns={'Away Team':'Team', 'Away Score':'Score', 'Home Score':'Opp Score'}, inplace=True)
 
+            standings_data = pd.concat([temp1,temp2], axis=0, ignore_index=True)
+            standings_data['Win'] = standings_data['Score'] > standings_data['Opp Score']
+            standings_data['Win'] = standings_data['Win'].map(int)
 
-        # Add in the pieces which make up the general template
-        doc.add(header(active_year=year))
-        scoreboard = df.loc[(df['Year'] == year) & (df['Week'] == i+1), ['Home Team','Home Score','Away Score','Away Team']].copy()
-        standings = df.loc[(df['Year'] == year) & (df['Week'] <= i+1)].copy()
+            teams = standings_data['Team'].unique()
+            records = []
 
-        temp1 = standings[['Home Team','Home Score','Away Score']].copy()
-        temp2 = standings[['Away Team','Away Score','Home Score']].copy()
-
-        temp1.rename(columns={'Home Team':'Team', 'Home Score':'Score', 'Away Score':'Opp Score'}, inplace=True)
-        temp2.rename(columns={'Away Team':'Team', 'Away Score':'Score', 'Home Score':'Opp Score'}, inplace=True)
-
-        standings_data = pd.concat([temp1,temp2], axis=0, ignore_index=True)
-        standings_data['Win'] = standings_data['Score'] > standings_data['Opp Score']
-        standings_data['Win'] = standings_data['Win'].map(int)
-
-        teams = standings_data['Team'].unique()
-        records = []
-
-        for team in teams:
-            wins = standings_data.loc[(standings_data['Team'] == team) & (standings_data['Win'] == 1), 'Win'].sum()
-            losses = i + 1 - wins
-            points_for = round(standings_data.loc[(standings_data['Team'] == team), 'Score'].sum(), 2)
-            points_against = round(standings_data.loc[(standings_data['Team'] == team), 'Opp Score'].sum(), 2)
-            records.append([team, wins, losses, points_for, points_against])
-        standings = pd.DataFrame(records, columns=['Team','Wins','Losses','Points For','Points Against']).sort_values(['Wins','Points For'], ascending=False)
+            for team in teams:
+                wins = standings_data.loc[(standings_data['Team'] == team) & (standings_data['Win'] == 1), 'Win'].sum()
+                losses = i + 1 - wins
+                points_for = round(standings_data.loc[(standings_data['Team'] == team), 'Score'].sum(), 2)
+                points_against = round(standings_data.loc[(standings_data['Team'] == team), 'Opp Score'].sum(), 2)
+                records.append([team, wins, losses, points_for, points_against])
+            standings = pd.DataFrame(records, columns=['Team','Wins','Losses','Points For','Points Against']).sort_values(['Wins','Points For'], ascending=False)
 
 
-        doc.add(content(title=f'{year} Week {i+1}', scoreboard=scoreboard, standings=standings))
-        doc_string = str(doc)
+            doc.add(content(title=f'{year} Week {i+1}', scoreboard=scoreboard, standings=standings))
+            doc_string = str(doc)
 
-        with open(f'fantasy-football-website/{year}/week-{i+1}.html','w') as file:
-            file.write(doc_string)
+            with open(f'fantasy-football-website/{year}/week-{i+1}.html','w') as file:
+                file.write(doc_string)
+
+
+
+def home_page():
+    doc = dominate.document(title='Fantasy Football')
+    doc.head.add(link(rel='stylesheet', href=f'{root}/style.css'))
+    doc.add(header(active_year='Home'))
+
+    content = div(_class='content')
+    content.add(h1('Documentation'))
+
+    documentation = object_('Documentation', data='/fantasy-football-website/Assets/Fantasy-Football.pdf', height='1000px', width='90%')
+    content.add(documentation)
+
+    doc.add(content)
+
+    with open(f'fantasy-football-website/index.html','w') as file:
+        file.write(doc.render())
+
+home_page()
+
+
