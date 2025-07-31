9f0339c3c56fcab7ba21670734955e2238d5bdfe
serdarevichar8 <serdarevichar@gmail.com>
2025-07-31 16:58:51 -0600

    Add Draft to Team Pages + Generalized JS Function
     - All draft picks by teach team now on team page
     - Added dropdown to the table, but to filter by year
     - Converted draftTeamFilter() to draftTableFilter(), removed reference to team/year and replaced with value
     -- Above could continue to generalize by taking an input of which ID to search for
     - Regenerated all affected pages

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function draftTeamFilter() {
-    const dropdownValue = document.getElementById('draft-team-filter').value
+function draftTableFilter() {
+    const dropdownValue = document.getElementById('draft-filter').value
     const rows = document.querySelectorAll('.content-container #league-draft-table tbody tr')
 
     rows.forEach(row => {
         const id = row.id
-        const team = id.split('-')[1]
-        if (team === dropdownValue || dropdownValue === 'all') {
+        const value = id.split('-')[1]
+        if (value === dropdownValue || dropdownValue === 'all') {
             row.style.display = ''
         } else {
             row.style.display = 'none'
