f1c627f3a6ea597328215dc86ae86ae8ecef012c
serdarevichar8 <serdarevichar@gmail.com>
2025-09-20 13:49:25 -0500

    Added weekly summary on home page
    On the home page now there will be a brief summary of the previous week. Currently shows:
     - Top performing bench players
     - Top performing players

--- python/home_page.py
+++ python/home_page.py
@@ -1,9 +1,9 @@
 from dominate.tags import *
 import pandas as pd
 
-from python import document, page_header, functions
+from python import document, page_header, functions, constants
 
-def home_content() -> div:
+def home_content(week: int) -> div:
     '''
     Function which creates the home page content
 
@@ -14,18 +14,6 @@ def home_content() -> div:
     container = div(_class='content')
     container.add(h1('Home'))
 
-    draft_order_df = pd.DataFrame(
-        {
-            'Team':['Andrew','Dante','Noah','Haris','Stolarz','McGwire','Nathan','Ethan','Michael','Kevin','Carter','Tyler'],
-            'Pick':[1,2,3,4,5,6,7,8,9,10,11,12]
-        }
-    )
-    draft_order_table = functions.df_to_table(draft_order_df)
-    draft_order_div = functions.content_container(
-        title='2025 Draft Order',
-        content=draft_order_table
-    )
-    
     payout_df = pd.DataFrame(
         {
             'Position':['1st','2nd','3rd','Reg Szn Champ'],
@@ -38,16 +26,35 @@ def home_content() -> div:
         content=payout_table
     )
 
-    container.add(draft_order_div)
     container.add(payout_div)
+
+
+    container.add(h2('Weekly Summary'))
+
+    year = 2025
+
+    data = constants.PLAYER_GAME_DATA.loc[(constants.PLAYER_GAME_DATA['Year'] == year) & (constants.PLAYER_GAME_DATA['Week'] == week)]
+
+    bench_df = data.loc[data['Slot Position'] == 'BE', ['Team','Player','Position','Points']].copy().sort_values('Points', ascending=False).head(5)
+    bench_table = functions.df_to_table(data=bench_df, table_id='topScoringBench')
+    bench_div = functions.content_container(title='Highest Scoring Bench Players', content=bench_table)
+
+    starters_df = data.loc[data['Slot Position'] != 'BE', ['Team','Player','Position','Points']].copy().sort_values('Points', ascending=False).head(5)
+    starters_table = functions.df_to_table(data=starters_df, table_id='topScoringStarters')
+    starters_div = functions.content_container(title='Highest Scoring Starters', content=starters_table)
+    
+    
+
+    container.add(bench_div)
+    container.add(starters_div)
 
     return container
 
-def home_page():
+def home_page(week: int):
     doc = document.document()
     doc.add(page_header.page_header(active_year='home'))
 
-    doc.add(home_content())
+    doc.add(home_content(week=week))
 
     with open('index.html','w') as file:
         file.write(doc.render())
