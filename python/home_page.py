8ca486415a8c5932459a84230cf5ee6b7216a9ba
serdarevichar8 <serdarevichar@gmail.com>
2025-09-20 14:39:08 -0500

    Load 2025 Week 2 Data
    Brought in updated data
    
    Made edits to home page weekly summary

--- python/home_page.py
+++ python/home_page.py
@@ -33,20 +33,24 @@ def home_content(week: int) -> div:
 
     year = 2025
 
-    data = constants.PLAYER_GAME_DATA.loc[(constants.PLAYER_GAME_DATA['Year'] == year) & (constants.PLAYER_GAME_DATA['Week'] == week)]
+    data = constants.PLAYER_GAME_DATA.loc[(constants.PLAYER_GAME_DATA['Year'] == year) & (constants.PLAYER_GAME_DATA['Week'] == week)].copy()
+    data['Proj Diff'] = round(data['Points'] - data['Projected Points'], 2)
 
     bench_df = data.loc[data['Slot Position'] == 'BE', ['Team','Player','Position','Points']].copy().sort_values('Points', ascending=False).head(5)
     bench_table = functions.df_to_table(data=bench_df, table_id='topScoringBench')
     bench_div = functions.content_container(title='Highest Scoring Bench Players', content=bench_table)
 
-    starters_df = data.loc[data['Slot Position'] != 'BE', ['Team','Player','Position','Points']].copy().sort_values('Points', ascending=False).head(5)
-    starters_table = functions.df_to_table(data=starters_df, table_id='topScoringStarters')
-    starters_div = functions.content_container(title='Highest Scoring Starters', content=starters_table)
-    
-    
+    under_df = data.loc[data['Slot Position'] != 'BE', ['Team','Player','Slot Position','Proj Diff']].copy().sort_values('Proj Diff', ascending=True).reset_index(drop=True).head(5)
+    under_table = functions.df_to_table(data=under_df, table_id='topUnderPerformers')
+    under_div = functions.content_container(title='Top Under Performers', content=under_table)
 
+    over_df = data.loc[data['Slot Position'] != 'BE', ['Team','Player','Slot Position','Proj Diff']].copy().sort_values('Proj Diff', ascending=False).reset_index(drop=True).head(5)
+    over_table = functions.df_to_table(data=over_df, table_id='topOverPerformers')
+    over_div = functions.content_container(title='Top Over Performers', content=over_table)
+    
     container.add(bench_div)
-    container.add(starters_div)
+    container.add(under_div)
+    container.add(over_div)
 
     return container
 
